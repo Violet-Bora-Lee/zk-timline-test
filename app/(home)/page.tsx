eeee8c82734cb80d020b828c5616b6ba91b1a019
Bora Lee <learnerbr@gmail.com>
2024-12-02 23:13:55 +0900

    Style background and text color

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,16 +3,16 @@ import Timeline from "@/components/Timeline";
 export default function Home() {
   return (
     <main>
-      <section className="py-20" id="roadmap">
+      <section className="py-20 bg-black text-white" id="roadmap">
         <div className="container mx-auto px-4">
           <div className="text-center mb-16">
-            <h2 className="text-4xl font-bold">
-              Zero Knowledge (zk) Events Timeline
-            </h2>
-            <p className="font-semibold mb-2">
-              A collaborative open-source timeline of significant zk events.
-              Contributions are welcome!
-            </p>
+        <h2 className="text-4xl font-bold">
+          Zero Knowledge (zk) Events Timeline
+        </h2>
+        <p className="font-semibold mb-2">
+          A collaborative open-source timeline of significant zk events.
+          Contributions are welcome!
+        </p>
           </div>
         </div>
       </section>
